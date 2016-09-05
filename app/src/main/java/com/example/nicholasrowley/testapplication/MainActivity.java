1ede165c4f815c48344a0c05caae440fcfd1625b
nicholasrowley <nick-1.03@hotmail.com>
2016-09-06 10:02:57 +1200

    Implemented some more API code.
    - still incomplete

--- app/src/main/java/com/example/nicholasrowley/testapplication/MainActivity.java
+++ app/src/main/java/com/example/nicholasrowley/testapplication/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.nicholasrowley.testapplication;
 
 import android.app.Activity;
+import android.app.Notification;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.graphics.PixelFormat;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -29,6 +33,9 @@ import com.dropbox.client2.session.AppKeyPair;
 import com.dropbox.client2.session.Session.AccessType;
 import com.dropbox.client2.android.AndroidAuthSession;
 import android.content.SharedPreferences.Editor;
+import com.dropbox.client2.session.TokenPair;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -84,12 +91,59 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             try {
                 session.finishAuthentication();
 
-                TokenPair tokens =session.getAccessTokenPair();
+                TokenPair tokens = session.getAccessTokenPair();
                 SharedPreferences prefs = getSharedPreferences(DROPBOX_NAME, 0);
-                Editor editor
+                Editor editor = prefs.edit();
+                editor.putString(ACCESS_KEY, tokens.key);
+                editor.putString(ACCESS_SECRET, tokens.secret);
+                editor.commit();
+
+                loggedIn(true);
             } catch (IllegalStateException e) {
                 Toast.makeText(this, "Error during Dropbox auth", Toast.LENGTH_SHORT).show();
             }
         }
+    }
+
+    private final Handler handler = new Handler() {
+        public void handleMessage(Message message) {
+            ArrayList<String> result = message.getData().getStringArrayList("data");
+
+            for(String fileName : result) {
+                TextView textView = new TextView(MainActivity.this);
+                container.addView(textView);
+            }
+        }
+    };
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.dbConnect:
+                if(isUserLoggedIn){
+                    dropboxApi.getSession().unlink();
+                    loggedIn(false);
+                } else {
+                    ((AndroidAuthSession) dropboxApi.getSession())
+                            .startAuthentication(MainActivity.this);
+                }
+                break;
+            case R.id.dbUploadFileBtn:
+                UploadFile uploadFile = new UploadFile(this, dropboxApi, DROPBOX_FILE_DIR);
+                uploadFile.execute();
+                break;
+            case R.id.dbListFilesBtn:
+                ListFiles listFiles = new ListFiles(dropboxApi, DROPBOX_FILE_DIR, handler);
+                listFiles.execute();
+        }
+    }
+
+    public void loggedIn(boolean userLoggedIn) {
+        isUserLoggedIn = userLoggedIn;
+        uploadFileBtn.setEnabled(userLoggedIn);
+        uploadFileBtn.setBackgroundColor(userLoggedIn ? Color.BLUE : Color.GRAY);
+        listFileBtn.setEnabled(userLoggedIn);
+        listFileBtn.setBackgroundColor(userLoggedIn ? Color.BLUE : Color.GRAY);
+        loginBtn.setText(userLoggedIn ? "Logout" : "Log in");
     }
 }
